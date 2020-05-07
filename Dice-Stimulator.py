e51a5818c64bdcd2c11f125574a76affb7709801
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2020-05-07 13:13:00 +0530

    Tic-Tac-Toe

---
+++ Dice-Stimulator.py
@@ -0,0 +1,54 @@
+import random
+def printDice(dice):
+    if dice == 1:
+        print(" ------- ")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print(" ------- ")
+    
+    if dice == 2:
+        print(" ------- ")
+        print("| 0     |")
+        print("|       |")
+        print("|     0 |")
+        print(" ------- ")
+
+    if dice == 3:
+        print(" ------- ")
+        print("| 0     |")
+        print("|   0   |")
+        print("|     0 |")
+        print(" ------- ")
+
+    if dice == 4:
+        print(" ------- ")
+        print("| 0   0 |")
+        print("|       |")
+        print("| 0   0 |")
+        print(" ------- ")
+
+    if dice == 5:
+        print(" ------- ")
+        print("| 0   0 |")
+        print("|   0   |")
+        print("| 0   0 |")
+        print(" ------- ")
+
+    if dice == 6:
+        print(" ------- ")
+        print("| 0 0 0 |")
+        print("| 0 0 0 |")
+        print("| 0 0 0 |")
+        print(" ------- ")
+        
+
+while True:
+    game = input("Do you want to roll the Dice (y/n) ?")
+
+    if game.lower() == 'y':
+        dice = random.randrange(1,7)
+        printDice(dice)
+    else:
+        print('Thank You!! We are closing your game.')
+        break
